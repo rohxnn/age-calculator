b9cefeb74aa79920d13a23eae5fdd47e9185a8fd
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-02-15 00:02:08 +0530

    chore(core): import reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+//module
 import { AppRoutingModule } from './app-routing.module';
+//component
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
