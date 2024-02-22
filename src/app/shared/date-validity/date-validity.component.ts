a36aca3efa18ed6a4c74712302a2e390ed6fc998
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-02-22 19:05:09 +0530

    feat(date-validity): add has error for valid date

--- src/app/shared/date-validity/date-validity.component.ts
+++ src/app/shared/date-validity/date-validity.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-date-validity',
-  template:  ` <ng-container *ngIf="control.invalid && (control.touched || control.dirty)" >
-               <small *ngIf="control.errors?.required" class="mt-3 text-danger">{{key}} is required</small>
-               <small *ngIf="(control.hasError('min') || control.hasError('max') || control.hasError('invalidDate')) && !control.errors?.required" class="mt-3 text-danger">{{key}} is invalid</small>
-               </ng-container>
+  template:  ` <ng-container *ngIf="control.invalid && (control.touched || control.dirty)">
+               <small *ngIf="control.errors?.['required']" class="mt-3 text-danger">{{ key }} is required</small>
+               <small *ngIf="(control.hasError('min') || control.hasError('max') || control.hasError('customError')) && !control.errors?.['required']" class="mt-3 text-danger">{{ key }} is invalid</small>
+               </ng-container>    
              `
 })
 export class DateValidityComponent {
 @Input()
-control: any;
+control: AbstractControl;
 @Input()
 key: any;
 }
