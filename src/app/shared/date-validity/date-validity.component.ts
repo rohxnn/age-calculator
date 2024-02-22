6d48bdabb37e59a8f6996405fa25e1c56f12810b
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-02-22 11:28:32 +0530

    refacto(date-validity): remove interpolations

--- src/app/shared/date-validity/date-validity.component.ts
+++ src/app/shared/date-validity/date-validity.component.ts
@@ -5,7 +5,6 @@ import { Component, Input } from '@angular/core';
   template:  ` <ng-container *ngIf="control.invalid && (control.touched || control.dirty)" >
                <small *ngIf="control.errors?.required" class="mt-3 text-danger">{{key}} is required</small>
                <small *ngIf="(control.hasError('min') || control.hasError('max') || control.hasError('invalidDate')) && !control.errors?.required" class="mt-3 text-danger">{{key}} is invalid</small>
-               {{control.hasError('invalidDate')}}
                </ng-container>
              `
 })
