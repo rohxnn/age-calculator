c528a66a25ba7e5c999ed0d3d80a37828ef607f5
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-02-23 12:18:02 +0530

    feat(date-validity): update logic

--- src/app/shared/custom-validator.validators.ts
+++ src/app/shared/custom-validator.validators.ts
@@ -15,8 +15,6 @@ export function customValidator(control: FormGroup) {
   //date validity check
   if (dayValue > daysInEachMonth[monthValue - 1]) {
       control.get('birth_day').setErrors({ customError: true }); 
-  } else {
-    control.get('birth_day').setErrors(null); 
-  }
+  } 
   return null;
 }
--- src/app/shared/date-validity/date-validity.component.ts
+++ src/app/shared/date-validity/date-validity.component.ts
@@ -5,7 +5,7 @@ import { AbstractControl } from '@angular/forms';
   selector: 'app-date-validity',
   template:  ` <ng-container *ngIf="control.invalid && (control.touched || control.dirty)">
                <small *ngIf="control.errors?.['required']" class="mt-3 text-danger">{{ key }} is required</small>
-               <small *ngIf="(control.hasError('min') || control.hasError('max') || control.hasError('customError')) && !control.errors?.['required']" class="mt-3 text-danger">{{ key }} is invalid</small>
+               <small *ngIf="(control.hasError('min') || control.hasError('max')) || (control.hasError('customError')) && !control.errors?.['required']" class="mt-3 text-danger">{{ key }} is invalid</small>
                </ng-container>    
              `
 })
